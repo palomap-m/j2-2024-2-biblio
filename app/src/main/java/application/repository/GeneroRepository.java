98a0c7762d8d6b3a5814089dd4ba4871e9a7984b
palomap-m <palomapasserani@gmail.com>
2024-09-13 19:49:44 +0000

    Aula 13/09/2024

---
+++ app/src/main/java/application/repository/GeneroRepository.java
@@ -0,0 +1,10 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, Long> {
+
+
+}
